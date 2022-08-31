0495513df8ed229afd17d92da913e8d7c85cfa53
guilhermeluciojardim <guilhermelucio@gmail.com>
2022-08-31 14:35:07 -0300

    commit room4
    - sandstorm created with script
    - room4 design initiated

---
+++ Joseph/Assets/Scripts/WindPushPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindPushPlayer : MonoBehaviour
+{
+    private Vector3 velocity;
+    [SerializeField] private float pushForce;
+    [SerializeField] private CharacterController controller;
+    
+
+    void OnCollisionStay(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Player")){
+            Debug.Log("Touched");
+            velocity.z = Mathf.Sqrt(pushForce * -2);
+            controller.Move(velocity * Time.deltaTime); 
+        }
+       
+    }
+}
